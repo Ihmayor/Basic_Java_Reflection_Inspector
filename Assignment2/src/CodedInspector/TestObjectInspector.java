3eb349b3261885a00982eb7fee551dfbf4029fa7
Irene Mayor <ihmayor@ucalgary.ca>
2017-10-26 18:14:14 -0600

    Added testInsepctInterface()

--- Assignment2/src/CodedInspector/TestObjectInspector.java
+++ Assignment2/src/CodedInspector/TestObjectInspector.java
@@ -16,91 +16,112 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class TestObjectInspector {
-
 	
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-
-	@Before
-	public void setUpStreams() {
-	    System.setOut(new PrintStream(outContent));
-	    System.setErr(new PrintStream(errContent));
-	}
-
-	@After
-	public void cleanUpStreams() {
-	    System.setOut(null);
-	    System.setErr(null);
-	    
-	    
-	}
+	
 	@Test
 	public void testInspectSuperClass() throws Exception {
+		//Arrange
+		//Byte Array output MUST BE started out here to work. Has been tested when placed in @Before and @After
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	    System.setOut(new PrintStream(outContent));
-
 		ObjectInspector inspector = new ObjectInspector();
-		
 		TestObject2 test = new TestObject2();
 		test.field1 = 0;
 		test.field2 = "Blah";
 		test.field4 = 2;
 		test.field5 = "test";
-		
-		
-		inspector.inspectSuperClass(test);
-		Class classObj = test.getClass();
-		Class superClass = classObj.getSuperclass();
-		
-		//Write Lines.
-		String expected = "Immediate Superclass:\tCodedInspector.TestObjectInspector$TestObject\n"
-				+ "Inspecting Superclass\n"
-				+ "===================================================\n"
-				+"Declared Constructor Amount: 1\n"
-				+ "Parameter Types: CodedInspector.TestObjectInspector\n"
-				+ "Constructor Modifier (int): 	1\n"
-				+ "Constructor Modifier (string): 	public\n\n\n\n\n\n\n"
-				+ "Declared Fields Amount: 3\n"
-				+ "Field Name:	field1\n"
-				+ "Field Value:\t"+test.field1+"\n"
-				+ "Field Name:	field2\n"
-				+ "Field Value:\t"+test.field2.hashCode()+"\n"
-				+ "Field Name:	this$0\n"
-				+ "Field Value:\t1121647253\n"
-				+ "=====================================================\n"
-				+"Exploring SuperClass of Super Class CodedInspector.TestObjectInspector$TestObject\n"
-				+"Immediate Superclass:	java.lang.Object\n"
-				+"Inspecting Superclass\n"
-				+"===================================================\n"
-				+"Method Name: 	finalize\n"
-				+"Method Name: 	wait\n"
-				+"Method Name: 	wait\n"
-				+"Method Name: 	wait\n"
-				+"Method Name: 	equals\n"
-				+"Method Name: 	toString\n"
-				+"Method Name: 	hashCode\n"
-				+"Method Name: 	getClass\n"
-				+"Method Name: 	clone\n"
-				+"Method Name: 	notify\n"
-				+"Method Name: 	notifyAll\n"
-				+"Method Name: 	registerNatives\n"
-				+"Declared Constructor Amount: 1\n"
-				+"Constructor Modifier (int): 	1\n"
-				+"Constructor Modifier (string): 	public\n"
-				+"\n\n\n\n\n\n"
-				+"Declared Fields Amount: 0\n"
-				+"This superclass extends no superclass\n"
-				+"======================================================\n"
-				+"Finished Inspecting SuperClass\n"
-				+"End Exploring Super Class of Super Class CodedInspector.TestObjectInspector$TestObject\n"
-				+"=====================================================\n"
-				+"======================================================\n"
-				+"Finished Inspecting SuperClass";
 
-		assertEquals(expected, outContent.toString());
+		String expected = "Immediate Superclass:\tCodedInspector.TestObjectInspector$TestObject"+System.getProperty("line.separator")
+		+ "Inspecting Superclass"+System.getProperty("line.separator")
+		+ "==================================================="+System.getProperty("line.separator")
+		+"Declared Constructor Amount: 1"+System.getProperty("line.separator")
+		+ "Parameter Types: CodedInspector.TestObjectInspector"+System.getProperty("line.separator")
+		+ "Constructor Modifier (int): 	1"+System.getProperty("line.separator")
+		+ "Constructor Modifier (string): 	public"
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+ "Declared Fields Amount: 3"+System.getProperty("line.separator")
+		+ "Field Name:	field1"+System.getProperty("line.separator")
+		+ "Field Value:\t"+test.field1+""+System.getProperty("line.separator")
+		+ "Field Name:	field2"+System.getProperty("line.separator")
+		+ "Field Value:\t"+test.field2.hashCode()+System.getProperty("line.separator")
+		+ "Field Name:	this$0"+System.getProperty("line.separator")
+		+ "Field Value:\t"+this.hashCode()+System.getProperty("line.separator")
+		+ "====================================================="+System.getProperty("line.separator")
+		+"Exploring SuperClass of Super Class CodedInspector.TestObjectInspector$TestObject"+System.getProperty("line.separator")
+		+"Immediate Superclass:	java.lang.Object"+System.getProperty("line.separator")
+		+"Inspecting Superclass"+System.getProperty("line.separator")
+		+"==================================================="+System.getProperty("line.separator")
+		+"Method Name: 	finalize"+System.getProperty("line.separator")
+		+"Method Name: 	wait"+System.getProperty("line.separator")
+		+"Method Name: 	wait"+System.getProperty("line.separator")
+		+"Method Name: 	wait"+System.getProperty("line.separator")
+		+"Method Name: 	equals"+System.getProperty("line.separator")
+		+"Method Name: 	toString"+System.getProperty("line.separator")
+		+"Method Name: 	hashCode"+System.getProperty("line.separator")
+		+"Method Name: 	getClass"+System.getProperty("line.separator")
+		+"Method Name: 	clone"+System.getProperty("line.separator")
+		+"Method Name: 	notify"+System.getProperty("line.separator")
+		+"Method Name: 	notifyAll"+System.getProperty("line.separator")
+		+"Method Name: 	registerNatives"+System.getProperty("line.separator")
+		+"Declared Constructor Amount: 1"+System.getProperty("line.separator")
+		+"Constructor Modifier (int): 	1"+System.getProperty("line.separator")
+		+"Constructor Modifier (string): 	public"+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+System.getProperty("line.separator")
+		+"Declared Fields Amount: 0"+System.getProperty("line.separator")
+		+"This superclass extends no superclass"+System.getProperty("line.separator")
+		+"======================================================"+System.getProperty("line.separator")
+		+"Finished Inspecting SuperClass"+System.getProperty("line.separator")
+		+"End Exploring Super Class of Super Class CodedInspector.TestObjectInspector$TestObject"+System.getProperty("line.separator")
+		+"====================================================="+System.getProperty("line.separator")
+		+"======================================================"+System.getProperty("line.separator")
+		+"Finished Inspecting SuperClass"+System.getProperty("line.separator");
 		
+		//Act
+		inspector.inspectSuperClass(test);		
+		
+		//Assert
+		assertEquals(expected, outContent.toString());
 	}
 	
+	@Test
+	public void testInspectInterface() throws Exception {
+		//Arrange
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	    System.setOut(new PrintStream(outContent));
+		ObjectInspector inspector = new ObjectInspector();
+
+		TestObject1 test =  new TestObject1();
+		inspector.inspectSuperInterface(test);
+	
+		String expected = "Implemented Interface: CodedInspector.TestObjectInspector$TestInterface"+System.getProperty("line.separator")
+				+"Inspecting Interface"+System.getProperty("line.separator")
+				+"==================================================="+System.getProperty("line.separator")
+				+"Method Name: 	blah"+System.getProperty("line.separator")
+				+"Declared Constructors Amount: 0"+System.getProperty("line.separator")
+				+"============================================================================="+System.getProperty("line.separator")
+				+System.getProperty("line.separator")
+				+System.getProperty("line.separator")
+				+"Declared Fields Amount: 0"+System.getProperty("line.separator")
+				+"============================================================================="+System.getProperty("line.separator")
+				+"======================================================"+System.getProperty("line.separator")
+				+"Finished Inspecting Interface"+System.getProperty("line.separator")
+				+"This superinterface implements no other superinterfaces"+System.getProperty("line.separator");
+		
+		assertEquals(expected, outContent.toString());
+	}
+	
+	////////////////////Test Objects/////////////////////////////
 	
 	public class TestObject{
 		public int field1;
